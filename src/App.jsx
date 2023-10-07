825c6aeb5f1a3e1d17836ff8df89a888dd407e36
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-08 00:07:54 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 // import pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Blog from './pages/Blog';
 import Support from './pages/Support';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
-      <Route path='/support' element={<Support />}/>
+      <Route path='/support' element={<Blog />}/>
     </Routes>
   )
 }
