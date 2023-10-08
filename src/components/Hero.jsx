e83f079b0e65dca2d9dc1d818e9a89fc5ee38f16
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-08 20:59:10 +0100

    in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,13 @@ function Hero() {
   return (
     <section className="py-20">
       <div className="homePage h-[95vh] w-full px-10">
-        <div className="flex flex-col justify-center w-full items-center h-full gap-y-7 mx-auto">
-          <h1 className="text-white text-center xl:max-w-6xl max-w-[80vh] xl:leading-none leading-[3.2rem] font-semibold text-5xl xl:text-[64px]">
+        <div className="flex flex-col justify-center w-full items-center h-full gap-y-8 mx-auto">
+          <h1 className="text-white text-center xl:max-w-6xl max-w-[80vh] md:leading-none leading-11 font-semibold text-4xl md:text-[64px]">
             Find your <span className="text-baddhia-orange">Perfect Match</span>
-            , <br /> <span className="text-baddhia-orange">Anywhere</span> in
+            ,<span className="text-baddhia-orange"> Anywhere</span> in
             the World.
           </h1>
-          <p className="text-white xl:max-w-[80vh] max-w-[60vh] text-center xl:text-base text-[14px]">
+          <p className="text-white xl:max-w-[80vh] max-w-[60vh] text-center xl:text-base md:text-[14px] text-[12px]">
             Finding the right person is tough, but we have made it easier.
             Whether you’re looking for love, friendship, or chat, our algorithm
             will connect you with someone who shares your interests and values.
