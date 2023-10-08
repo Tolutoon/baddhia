68f8db0735d0fac061c7ac7442277f42acd0aef3
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-08 21:21:06 +0100

    in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <section className="py-20">
-      <div className="homePage h-[95vh] w-full px-10">
+      <div className="homePage h-[95vh] w-full px-10 flex-nowrap">
         <div className="flex flex-col justify-center w-full items-center h-full gap-y-8 mx-auto">
           <h1 className="text-white text-center xl:max-w-6xl max-w-[80vh] md:leading-none leading-11 font-semibold text-4xl md:text-[64px]">
             Find your <span className="text-baddhia-orange">Perfect Match</span>
