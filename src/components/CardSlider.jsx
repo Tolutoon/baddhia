1022f08e51971adf3ed14b1da97876c0bb3e7048
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-08 20:35:42 +0100

    in progress

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import Data from "../Data";
 
 function CardSlider() {
   return (
-    <div className="h-fit w-full pt-20 relative px-5 my-10 py-20 xl:hidden flex flex-row duration-500">
+    <div className="h-fit w-full pt-20 relative px-5 xl:my-10 my-5 py-20 xl:hidden flex flex-row duration-500">
       {Data.map((elem) => {
         const { id, description, name } = elem;
         return (
           <div
-            className="card flex flex-col gap-[30px] w-[350px] h-fit py-10 px-5 mb-10 justify-center mx-5 items-center flex-shrink-0 bg-white"
+            className="card flex flex-col gap-[30px] w-[350px] h-fit py-10 px-5 mb-20 justify-center mx-5 items-center flex-shrink-0 bg-white"
             key={id}
           >
             <span className="text-[64px] text-baddhia-orange">❝</span>
@@ -20,11 +20,13 @@ function CardSlider() {
           </div>
         );
       })}
-      <div className="absolute -translate-x-0 translate-y-[-50%] left-[40%] bottom-[5%] p-2 text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
-        <BsChevronCompactLeft />
-      </div>
-      <div className="absolute -translate-x-0 translate-y-[-50%] p-2 right-[40%] bottom-[5%] text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
-        <BsChevronCompactRight/>
+      <div className="flex">
+        <div className="absolute -translate-x-0 translate-y-[-50%] left-[35%] bottom-[5%] p-2 md:hidden flex text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
+          <BsChevronCompactLeft size={24} />
+        </div>
+        <div className="absolute -translate-x-0 translate-y-[-50%] p-2 right-[35%] bottom-[5%] md:hidden flex text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
+          <BsChevronCompactRight size={24} />
+        </div>
       </div>
     </div>
   );
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,33 +2,34 @@ import React from "react";
 import Data from "../Data";
 import CardSlider from "./CardSlider";
 
-
-function Testimonials () {
+function Testimonials() {
   return (
     <section className="testimonal-backDrop px-10">
       <div className="flex flex-col justify-center w-full xl:items-center">
-        <h2 className="xl:text-3xl text-2xl font-semibold mx-auto xl:mb-20">What our <span className="text-baddhia-orange">users</span> say</h2>
-    <div className="justify-center relative xl:flex flex-col h-fit md:flex-row p-10 hidden">
-      {Data.map((elem) => {
-        const { id, description, name } = elem;
-        return (
-          <div
-            className="card flex flex-col gap-[32px] w-[350px] h-fit py-10 px-5 mb-10 justify-center mx-5 items-center flex-shrink-0 bg-white"
-            key={id}
-          >
-            <span className="text-[64px] text-baddhia-orange">❝</span>
-            <p className="text-[12px] text-gray-500 text-center">{description}</p>
-            <h3 className="font-semibold">{name}</h3>
-          </div>
-        );
-      })}
-    </div>
-    <div>
-     <CardSlider/>
-    </div>
+        <h2 className="xl:text-3xl text-2xl font-semibold mx-auto xl:mb-20">
+          What our <span className="text-baddhia-orange">users</span> say
+        </h2>
+        <div className="justify-center relative xl:flex flex-col h-fit md:flex-row p-10 hidden">
+          {Data.map((elem) => {
+            const { id, description, name } = elem;
+            return (
+              <div
+                className="card flex flex-col gap-[32px] w-[350px] h-fit py-10 px-5 mb-10 justify-center mx-5 items-center flex-shrink-0 bg-white"
+                key={id}
+              >
+                <span className="text-[64px] text-baddhia-orange">❝</span>
+                <p className="text-[12px] text-gray-500 text-center">
+                  {description}
+                </p>
+                <h3 className="font-semibold">{name}</h3>
+              </div>
+            );
+          })}
+        </div>
+        <CardSlider />
       </div>
     </section>
   );
-};
+}
 
 export default Testimonials;
