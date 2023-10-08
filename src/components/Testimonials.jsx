32bb6f662d4a1ae5fbc9fd007df4dba5af7703b5
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-08 08:56:18 +0100

    in progress

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Testimonials() {
   return (
-    <div className='testimonal-backDrop h-[761px] w-full pt-40'>Testimonals</div>
-  )
+    <div className="testimonal-backDrop h-[761px] w-full pt-40 relative">
+      <div className="absolute gap-[40px] w-[350px] h-[490px] flex-shrink-0 bg-white">
+          <img src="" alt="" />
+          <p className="text-base text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+            dolorum dolor alias ipsum iure odit voluptas molestias asperiores,
+            necessitatibus explicabo aperiam facilis labore. Eum sunt distinctio
+            autem iste consequuntur tempora. Tempore natus est numquam nobis
+            autem minus deserunt ducimus, vero aut eum perferendis quidem sint
+            impedit excepturi aliquid? Aperiam, distinctio nam! Debitis, in
+            beatae. Laborum minus totam voluptatibus aliquid asperiores?
+          </p>
+          <h3 className="font-semibold">James Pattinson</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonials;
