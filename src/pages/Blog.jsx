c1f08f7a3fce152536a4fe28e1edc54758537316
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-10 07:32:45 +0100

    in progress

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import BlogHeader from '../components/BlogHeader';
-import BlogImg from '../components/BlogImg';
-import BlogWriteup from '../components/BlogWriteup';
-
-
 
 function Blog() {
-    
   return (
-    <>
-      <BlogHeader />
-      <BlogImg />
-      <BlogWriteup />
-    </>
-    
+    <div>Blog</div>
   )
 }
 
