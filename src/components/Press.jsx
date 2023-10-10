28b072b1681906350b4e174da225e2f84f3e0bcf
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-10 21:13:37 +0100

    in progress

--- src/components/Press.jsx
+++ src/components/Press.jsx
@@ -29,16 +29,10 @@ function Press() {
         Baddhia in the <span className="text-baddhia-orange">press</span>
       </h2>
       <div className="flex md:flex-row flex-col justify-center items-center gap-10">
-        {pressData.map((item) => {
-          const divStyle = {
-            backgroundImage: `url(${item.backgroundUrl})`,
-          };
-
-          return (
-            <div style={divStyle} className="md:h-[350px] md:w-[323px] h-[280px] w-[263px] bg-cover bg-no-repeat relative shadow-md rounded-2xl" key={item.id}>
+            <div className="md:h-[350px] bg-[url('/images/techCabal.png')] md:w-[323px] h-[280px] w-[263px] bg-cover bg-no-repeat relative shadow-md rounded-2xl">
               <div className="flex absolute bg-[#FFF6F2] md:w-[323px] h-[40%] w-[263px] bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center">
-                <img className="w-[130px]" src={item.logoUrl} alt="" />
-                <a href={item.url} target="_blank" rel="noopener noreferrer"> {/* Added target="_blank" for opening links in a new tab */}
+                <img className="w-[130px]" src='/images/techCabalLogo.svg' alt="" />
+                <a href='https://techcabal.com/2022/10/27/baddhia-launches-app-aims-to-inspire-bliss-in-every-romantic-relationship-across-africa/' target="_blank" rel="noopener noreferrer"> {/* Added target="_blank" for opening links in a new tab */}
                   <button
                     type="submit"
                     className="bg-baddhia-orange p-4 px-8 rounded-[16px] font-semibold md:text-base text-[12px] text-white transition-all duration-300 hover:scale-105"
@@ -48,8 +42,6 @@ function Press() {
                 </a>
               </div>
             </div>
-          );
-        })}
       </div>
     </section>
   );
