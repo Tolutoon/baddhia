bcd1a9df0f4b201cebb97ac46d6b5c9db78793cd
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-10 07:28:59 +0100

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
-      <Route path='/blog' element={<Blog />}/>
     </Routes>
   )
 }
