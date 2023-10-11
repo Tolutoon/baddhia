6c2df58c3f4fd5c5e6423a8b382520a67296096a
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-11 06:58:00 +0100

    push

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import CardBenefits from '../components/CardBenefits';
+
 
 function About() {
   return (
     <div>
       <Header />
+      <CardBenefits />
+
     </div>
   )
 }
