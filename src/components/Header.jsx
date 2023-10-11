1d7048d4060ecb8a76fd92e890ffc924940745ff
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-11 05:41:02 +0100

    in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 // import assets
@@ -10,15 +10,21 @@ import { AiOutlineClose } from "react-icons/ai";
 function Header() {
   const [toggleMenu, setToggleMenu] = useState(false);
 
+
+
   return (
     <header className="w-full bg-white shadow-md font-semibold fixed z-10 transition-all duration-300">
-      <div className="flex justify-between items-center md:py-4 xl:px-40 px-10 py-2">
-        <Link to={'/'} className="cursor-pointer">
+      <div
+        className={`flex ${
+          toggleMenu ? "" : "justify-between"
+        } items-center md:py-4 xl:px-40 px-10 py-2`}
+      >
+        <Link to={"/"} className="cursor-pointer">
           <img src={Logo} alt="" style={{ width: 51, height: 50 }} />
         </Link>
 
         <nav>
-          <ul className="md:flex gap-x-7 uppercase hidden transition">
+          <ul className="md:flex gap-x-7 uppercase hidden transition list-none">
             <li className="text-baddhia-orange text-base">
               <a href="/about">About us</a>
             </li>
@@ -36,22 +42,32 @@ function Header() {
           </ul>
           <div className="flex relative">
             {toggleMenu ? (
-              <AiOutlineClose fontSize={28} />
+              <AiOutlineClose
+                fontSize={28}
+                className="text-baddhia-orange cursor-pointer md:hidden"
+                onClick={() => setToggleMenu(false)}
+              />
             ) : (
               <BiMenuAltRight
                 fontSize={28}
                 className="md:hidden text-baddhia-orange cursor-pointer"
+                onClick={() => setToggleMenu(true)}
               />
             )}
             {toggleMenu && (
-              <ul className="flex flex-col gap-x-7 uppercase transition">
-                <li className="text-baddhia-orange text-base">
+              <ul className="flex flex-col uppercase transition list-none rounded-md fixed md:hidden top-0 w-full h-[50vh] p-3 justify-start bg-white shadow-2xl">
+                <AiOutlineClose
+                  fontSize={28}
+                  onClick={() => setToggleMenu(false)}
+                  className="cursor-pointer text-baddhia-orange"
+                />{" "}
+                <li className="text-baddhia-orange text-xl w-full my-2 mt-10 flex items-end">
                   <a href="/about">About us</a>
                 </li>
-                <li className="text-baddhia-orange text-base">
+                <li className="text-baddhia-orange text-xl w-full my-2 flex items-end">
                   <a href="/support">Support</a>
                 </li>
-                <li className="text-baddhia-orange text-base">
+                <li className="text-baddhia-orange text-xl w-full my-2 flex items-end">
                   <a
                     href="https://play.google.com/store/apps/details?id=com.baddhia.app"
                     target="_blank"
--- src/components/JoinCard.jsx
+++ src/components/JoinCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function JoinCard() {
   return (
-    <div className="bg-[url('/images/joinBack.png')] h-[80vh] w-full bg-center">
+    <div className="bg-[url('/images/joinBack.png')] h-[80vh] w-full bg-center bg-no-repeat">
         <h2>Join the Baddhia Community</h2>
     </div>
   )
