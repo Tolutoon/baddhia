aeecd5396a1a7e82f29e091d5cc4bbc949284493
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-11 07:35:32 +0100

    push

--- src/components/JoinCard.jsx
+++ src/components/JoinCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function JoinCard() {
   return (
     <div className="bg-[url('/images/joinBack.png')] h-[80vh] w-full bg-center bg-no-repeat flex xl:items-center justify-center pt-20">
-      <div className="flex flex-col md:max-w-[40vw]">
+      <div className="flex flex-col md:max-w-[40vw] gap-8">
 
-      <h2 className="xl:text-6xl text-4xl font-semibold text-left xl:leading-[69px] text-white uppercase">Join the<br/> <span className="text-baddhia-orange">Baddhia</span> <br/> Community</h2>
-      <p className="text-white w-[442px] leading-[28px]">
+      <h2 className="xl:text-6xl text-4xl font-semibold text-left xl:leading-[59px] text-white uppercase">Join the<br/> <span className="text-baddhia-orange">Baddhia</span> <br/> Community</h2>
+      <p className="text-white md:w-[442px] w-[80vw] leading-[28px]">
         We exist to inspire bliss in every romantic relationship. Be sure that
         all of us at Baddhia are committed to making you find love and build
         intimacy with your significant other. Join our exclusive mailing list to
