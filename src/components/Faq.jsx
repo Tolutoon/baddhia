2a7b34723f1a3d2fdd2b0f302d03326e69cbb8bc
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-12 06:20:45 +0100

    about page

---
+++ src/components/Faq.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Faq() {
+  return (
+    <section className='pt-40'>
+        <div>
+            <h1 className='md:text-4xl text-center font-semibold'>Frequently Asked Questions</h1>
+        </div>
+    </section>
+  )
+}
+
+export default Faq
--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Faq from '../components/Faq';
 
 function Support() {
   return (
     <div>
       <Header />
+      <Faq/>
     </div>
   )
 }
