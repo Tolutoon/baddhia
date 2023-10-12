0d8313e6b409d8140a5a4c6a56329a18cac737e1
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-12 17:50:53 +0100

    push

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,14 +4,13 @@ import data from '../faqData';
 function Faq() {
   return (
     <div className='md:pt-40 pt-20'>
-      <h1>Faq</h1>
+      <h1 className='md:text-4xl text-2xl text-center mb-10 text-baddhia-orange'>Frequently Asked Questions</h1>
       {data.map((elem)=>{
         return (
-          <>
-          
-          <div>{elem.title}</div>
+          <div className='bg-[#e9e9e9] p-5 rounded-md'>     
+          <div className='font-semibold'>{elem.title}</div>
           <div>{elem.info}</div>
-          </>
+          </div>
         )
       })}
     </div>
