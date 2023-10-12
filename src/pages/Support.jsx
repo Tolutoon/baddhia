1fac24aca7f127d8c7c8a4da39fcf4e8f66990cb
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-12 21:04:13 +0100

    commit

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -26,12 +26,12 @@ function Support() {
             <p>+234 706 207 2194</p>
           </div>
           </div>
-          <form className='flex flex-col w-[70vw] md:w-[40vw] gap-8'>
-            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px]' placeholder='name' type='name'/>
-            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px]' placeholder='email' type='name'/>
-            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px]' placeholder='subject' type='name'/>
-            <textarea className=' border-baddhia-orange border-2 h-[120px] rounded-xl'/>
-            <button className='bg-red-300 w-[190px]'>Send Message</button>
+          <form className='flex flex-col w-[70vw] md:w-[40vw] gap-8 mb-10'>
+            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px] px-4' placeholder='name' type='name'/>
+            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px] px-4' placeholder='email' type='name'/>
+            <input className=' border-baddhia-orange border-2 rounded-xl h-[40px] px-4' placeholder='subject' type='name'/>
+            <textarea className=' border-baddhia-orange border-2 h-[120px] rounded-xl px-4'/>
+            <button className='bg-baddhia-orange p-4 px-14 rounded-[16px] font-semibold text-base text-white w-fit'>Send Message</button>
           </form>
         </div>
       </div>
