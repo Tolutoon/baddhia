613bc95fae1c7dae16d47d7e9c549979f61117ee
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-12 08:01:58 +0100

    background image fix

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -3,8 +3,8 @@ import background from '/images/background-image.png';
 
 function BackgroundImage() {
   return (
-    <div className='h-screen w-full'>
-        <img src={background} className='h-[100vh] w-[100vw] bg-cover' alt=''/>
+    <div className='h-[100vh] w-full'>
+        <img src={background} className='h-[100vh] w-[100vw] object-cover' alt=''/>
     </div>
   )
 }
