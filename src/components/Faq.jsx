111a3422a30cbcccc142c165c3a1513af075a458
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-12 18:31:16 +0100

    push

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -16,14 +16,14 @@ function Faq() {
   };
 
   return (
-    <div className="md:pt-40 pt-20 md:px-40 px-20">
+    <div className="md:pt-40 pt-20 md:px-40 md:px-20">
       <h1 className="md:text-4xl text-2xl text-center mb-10 text-baddhia-orange">
         Frequently Asked Questions
       </h1>
       {items.map((elem) => (
         <div className="p-5 rounded-md flex items-center justify-center" key={elem.id}>
           <div
-            className={`flex flex-col bg-[#e9e9e9] rounded-xl p-5 md:w-[60vw] group ${
+            className={`flex flex-col bg-[#e9e9e9] rounded-xl p-5 md:w-[60vw] w-[80vw] group ${
               elem.active === 1 ? "is-active" : ""
             }`}
           >
@@ -36,7 +36,7 @@ function Faq() {
               </div>
               <div className="font-semibold group-[.is-active]:font-bold">{elem.title}</div>
             </div>
-            <div className="overflow-hidden max-h-0 group-[.is-active]:max-h-[120px]">
+            <div className="overflow-hidden max-h-0 group-[.is-active]:max-h-[120px] text-gray-500">
               {elem.info}
             </div>
           </div>
