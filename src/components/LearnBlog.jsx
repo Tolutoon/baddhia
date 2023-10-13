d7c3bcd2b1c9a06d8187c3b75404a60461e5d67c
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-13 07:49:29 +0100

    commit

--- src/components/LearnBlog.jsx
+++ src/components/LearnBlog.jsx
@@ -7,7 +7,7 @@ function LearnBlog() {
       <h1 className="text-center font-semibold md:text-3xl text-[22px] mb-10">Learn <span className="text-baddhia-orange">More</span> About our Blogs</h1>
       <div className=" bg-[url('/images/dummyImg.png')]  h-[470px] w-[293px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
         <div className="flex absolute bg-[#FFF6F2] w-fit bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center md:p-4 p-2">
-          <div className="w-[95%]">
+          <div className="w-[90%]">
             <h3 className="text-baddhia-orange font-semibold mb-4">More Then Romance: Building Lasting Friendships on Baddhia</h3>
             <p className="text-gray-400 text-[12px] mb-4">
               Aenean eleifend ante maecenas pulvinar montes lorem et pede dis
