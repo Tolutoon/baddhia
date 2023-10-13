5dbf755707e942f132c22524c46c6b5368d10c80
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-13 07:32:09 +0100

    commit

--- src/components/LearnBlog.jsx
+++ src/components/LearnBlog.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function LearnBlog() {
   return (
-    <div className="h-fit px-20 md:px-40 mb-20">
-      <h1 className="text-center font-semibold text-3xl mb-10">Learn <span className="text-baddhia-orange">More</span> About our Blogs</h1>
-      <div className=" bg-[url('/images/dummyImg.png')]  h-[480px] w-[283px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
+    <div className="h-fit md:px-40 mb-20 flex flex-col justify-center items-center">
+      <h1 className="text-center font-semibold md:text-3xl text-[22px] mb-10">Learn <span className="text-baddhia-orange">More</span> About our Blogs</h1>
+      <div className=" bg-[url('/images/dummyImg.png')]  h-[470px] w-[283px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
         <div className="flex absolute bg-[#FFF6F2] w-fit bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center p-4">
           <div className="w-[90%]">
             <h3 className="text-baddhia-orange font-semibold mb-4">More Then Romance: Building Lasting Friendships on Baddhia</h3>
@@ -14,10 +14,10 @@ function LearnBlog() {
               dolor pretium donec dictum. Vici consequat justo enim. Venenatis
               eget adipiscing luctus lorem.
             </p>
-            <div className="flex">
+            <div className="flex gap-4">
               <div className="flex flex-col">
-                <span>Fletcher James</span>
-                <span>June 28, 2013</span>
+                <span className="text-[12px]">Fletcher James</span>
+                <span className="text-[12px]">June 28, 2013</span>
               </div>
 
               <Link to={"/blogContent"}>
