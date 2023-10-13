209b65aa1a7d435819fd8f344ca6948b07568464
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-13 07:48:36 +0100

    commit

--- src/components/LearnBlog.jsx
+++ src/components/LearnBlog.jsx
@@ -6,15 +6,15 @@ function LearnBlog() {
     <div className="h-fit md:px-40 mb-20 flex flex-col justify-center items-center">
       <h1 className="text-center font-semibold md:text-3xl text-[22px] mb-10">Learn <span className="text-baddhia-orange">More</span> About our Blogs</h1>
       <div className=" bg-[url('/images/dummyImg.png')]  h-[470px] w-[293px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
-        <div className="flex absolute bg-[#FFF6F2] w-fit bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center p-4">
-          <div className="w-[90%]">
+        <div className="flex absolute bg-[#FFF6F2] w-fit bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center md:p-4 p-2">
+          <div className="w-[95%]">
             <h3 className="text-baddhia-orange font-semibold mb-4">More Then Romance: Building Lasting Friendships on Baddhia</h3>
             <p className="text-gray-400 text-[12px] mb-4">
               Aenean eleifend ante maecenas pulvinar montes lorem et pede dis
               dolor pretium donec dictum. Vici consequat justo enim. Venenatis
               eget adipiscing luctus lorem.
             </p>
-            <div className="flex gap-8">
+            <div className="flex md:gap-4 gap-8 ">
               <div className="flex flex-col">
                 <span className="text-[14px] font-semibold">Fletcher James</span>
                 <span className="text-[12px]">June 28, 2013</span>
@@ -25,7 +25,7 @@ function LearnBlog() {
                 {/* Added target="_blank" for opening links in a new tab */}
                 <button
                   type="submit"
-                  className="bg-baddhia-orange p-2 md:px-8 px-4 rounded-[16px] font-semibold text-[12px] text-white transition-all duration-300 hover:scale-105"
+                  className="bg-baddhia-orange p-2 md:px-8 px-6 rounded-[16px] font-semibold text-[12px] text-white transition-all duration-300 hover:scale-105"
                 >
                   Read More
                 </button>
