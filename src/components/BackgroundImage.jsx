6f6ac663c51ad91ea355041e018f17bf957380c0
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-13 06:58:49 +0100

    commit

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -4,7 +4,7 @@ import background from '/images/background-image.png';
 function BackgroundImage() {
   return (
     <div className='h-[100vh] w-full'>
-        <img src={background} className='h-[100vh] w-[100vw] object-cover' loading='lazy' alt=''/>
+        <img src={background} className='h-[100vh] w-[100vw] object-fit' loading='lazy' alt=''/>
     </div>
   )
 }
