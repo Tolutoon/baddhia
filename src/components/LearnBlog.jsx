65ad0d161a9ec9941ea9cda402554bae04c52ac6
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-13 07:35:08 +0100

    commit

--- src/components/LearnBlog.jsx
+++ src/components/LearnBlog.jsx
@@ -5,7 +5,7 @@ function LearnBlog() {
   return (
     <div className="h-fit md:px-40 mb-20 flex flex-col justify-center items-center">
       <h1 className="text-center font-semibold md:text-3xl text-[22px] mb-10">Learn <span className="text-baddhia-orange">More</span> About our Blogs</h1>
-      <div className=" bg-[url('/images/dummyImg.png')]  h-[470px] w-[283px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
+      <div className=" bg-[url('/images/dummyImg.png')]  h-[470px] w-[293px] bg-cover bg-center bg-no-repeat relative shadow-md rounded-2xl">
         <div className="flex absolute bg-[#FFF6F2] w-fit bottom-0 flex-col gap-[16px] rounded-2xl justify-center items-center p-4">
           <div className="w-[90%]">
             <h3 className="text-baddhia-orange font-semibold mb-4">More Then Romance: Building Lasting Friendships on Baddhia</h3>
@@ -16,7 +16,7 @@ function LearnBlog() {
             </p>
             <div className="flex gap-4">
               <div className="flex flex-col">
-                <span className="text-[12px]">Fletcher James</span>
+                <span className="text-[14px] font-semibold">Fletcher James</span>
                 <span className="text-[12px]">June 28, 2013</span>
               </div>
 
