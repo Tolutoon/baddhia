b0c18a1b64859780d608c1354fafcba79b562eaf
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-14 02:03:50 +0100

    nav bar fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,22 @@ export default {
     extend: {
       colors: {
         'baddhia-orange': '#fc5000',
-        'B-writeup': "#FFD9C8"
-      }
+      },
+    },
+    keyframes: {
+      "slide-in": {
+        "0%": {
+          "-webkit-transform": "translateX(120%)",
+          transform: "translateX(120%)",
+        },
+        "100%": {
+          "-webkit-transform": "translateX(0%)",
+          transform: "translateX(0%)",
+        },
+      },
+    },
+    animation: {
+      "slide-in": "slide-in 0.5s ease-out",
     },
   },
   plugins: [],
