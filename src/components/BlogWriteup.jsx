f85d00f66063aca3571e90eaaf845708ec43aa99
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2023-10-15 09:59:54 +0100

    Learn more btn update

--- src/components/BlogWriteup.jsx
+++ src/components/BlogWriteup.jsx
@@ -22,7 +22,7 @@ function BlogWriteup() {
                     <h2 className="font-bold mt-3">Anchor's Name</h2>
                     <div className="flex justify-between mt-2">
                         <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
                             <Link to="/BlogContent">Read more</Link>
                         </div>
                     </div>
@@ -39,7 +39,7 @@ function BlogWriteup() {
                     <h2 className="font-bold mt-3">Anchor's Name</h2>
                     <div className="flex justify-between mt-2">
                         <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
                             <Link to="/">Read more</Link>
                         </div>
                     </div>
@@ -57,7 +57,7 @@ function BlogWriteup() {
                     <h2 className="font-bold mt-3">Anchor's Name</h2>
                         <div className="flex justify-between mt-2">
                             <p className="text-xs">DD-MM-YYYY</p>
-                            <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                            <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
                             <Link to="/">Read more</Link>
                         </div>
                         </div>
