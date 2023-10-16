aee3a994f61d3a55dbd4a86deff379824e692adb
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-16 13:18:27 +0100

    in progress

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -4,17 +4,24 @@ import hero from "/images/background-image.png";
 function Heros() {
   return (
     <div className='relative'>
-            <img className='w-full' src={hero} alt="" />
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
-              <p className='lg:text-5xl font-bold sm:text-2xl text-l mt-5 md:text-3xl w-full mb-0 md:mb-3 m-auto md:font-bold'>Find Your <span className="text-baddhia-orange">Perfect Match, 
+      <div className="h-[10vh]"></div>
+            <img className='w-full h-[40vh]' src={hero} alt="" />
+            <div className='absolute top-1/2 min-w-[300px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
+              <p className='lg:text-5xl font-semibold text-3xl text-l mt-5 md:text-3xl w-full mb-2 md:mb-3 m-auto pt-10  md:font-bold'>Find Your <span className="text-baddhia-orange">Perfect Match, 
                 Anywhere</span> in the World.
               </p>
               <p className="w-full text-xs md:text-sm mb-2">Finding the right person is tough, but we have made it easier. Whether you’re looking for love, friendship, or chat, our algorithm will connect you with someone who shares your interests and values.</p>
-              <a href="https://play.google.com/store/apps/details?id=com.baddhia.app">
-              <button
-               className='bg-baddhia-orange md:py-3 mb-6 md:px-10 md:text-sm text-xs py-2 px-4 rounded-full'>Download Now
-               </button>
-               </a>
+              <a
+          href="https://play.google.com/store/apps/details?id=com.baddhia.app"
+          target="_blank"
+        >
+          <button
+            type="submit"
+            className="bg-baddhia-orange p-4 xl:px-12 px-6 rounded-[16px] font-semibold xl:text-base text-[10px] text-white transition-all duration-300 hover:scale-105"
+          >
+            Download Now
+          </button>
+        </a>
             </div>
           </div>
   );
