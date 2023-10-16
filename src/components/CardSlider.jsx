68fd3df5c8bc8a3c140d0ff831bdabb6750b6735
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-16 15:20:24 +0100

    in progress

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from 'swiper/core';
+
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import Data from "../Data";
 
+SwiperCore.use([Navigation, Pagination, Scrollbar]);
+
 function CardSlider() {
+  const swiperRef = React.useRef(null);
+
+  const slideToNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext();
+    }
+  };
+
+  const slideToPrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev();
+    }
+  };
+
   return (
     <div className="overflow-hidden">
       <Swiper
+
         spaceBetween={50}
         slidesPerView={1}
+
         onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
         {Data.map((item) => (
           <SwiperSlide key={item.id}>
@@ -29,10 +56,10 @@ function CardSlider() {
           </SwiperSlide>
         ))}
         <div className="flex mt-20">
-          <div className="absolute -translate-x-0 translate-y-[-50%] left-[35%] md:left-[45%] bottom-[5%] p-2 text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white shadow-md">
+          <div className="absolute -translate-x-0 translate-y-[-50%] left-[35%] md:left-[45%] bottom-[5%] p-2 text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white shadow-md" onClick={slideToPrev}>
             <BsChevronCompactLeft size={24} />
           </div>
-          <div className="absolute -translate-x-0 translate-y-[-50%] p-2 right-[35%] md:right-[45%] bottom-[5%] text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white shadow-md">
+          <div className="absolute -translate-x-0 translate-y-[-50%] p-2 right-[35%] md:right-[45%] bottom-[5%] text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white shadow-md" onClick={slideToNext}>
             <BsChevronCompactRight size={24} />
           </div>
         </div>
@@ -40,33 +67,6 @@ function CardSlider() {
     </div>
   );
 
-  {
-    /*<div className="h-fit pt-20 relative xl:my-10 my-5 py-20  xl:hidden flex flex-row duration-500">
-      {Data.map((item) => {
-        const { id, description, name } = item;
-        return (
-          <div
-            className={`card flex flex-col gap-[30px] w-[320px] h-[450px] py-10 px-5 mb-20 justify-center mx-5 items-center flex-shrink-0 bg-white`}
-            key={id}
-          >
-            <span className="text-[54px] text-baddhia-orange">❝</span>
-            <p className="text-[12px] text-gray-500 text-center w-[80%]">
-              {description}
-            </p>
-            <h3 className="font-semibold">{name}</h3>
-          </div>
-        );
-      })}
-      <div className="flex">
-        <div className="absolute -translate-x-0 translate-y-[-50%] left-[35%] bottom-[5%] p-2 text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
-          <BsChevronCompactLeft size={24} />
-        </div>
-        <div className="absolute -translate-x-0 translate-y-[-50%] p-2 right-[35%] bottom-[5%] text-2xl cursor-pointer rounded-full text-baddhia-orange bg-white">
-          <BsChevronCompactRight  size={24} />
-        </div>
-      </div>
-    </div>*/
-  }
 }
 
 export default CardSlider;
