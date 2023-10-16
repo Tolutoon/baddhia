490cf7d91e063b795c36e80da5964ad0be908090
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-16 13:22:05 +0100

    in progress

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -4,20 +4,20 @@ import hero from "/images/background-image.png";
 function Heros() {
   return (
     <div className='relative'>
-      <div className="h-[10vh]"></div>
+      <div className="h-[7vh]"></div>
             <img className='w-full h-[40vh]' src={hero} alt="" />
             <div className='absolute top-1/2 min-w-[300px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white'>
               <p className='lg:text-5xl font-semibold text-3xl text-l mt-5 md:text-3xl w-full mb-2 md:mb-3 m-auto pt-10  md:font-bold'>Find Your <span className="text-baddhia-orange">Perfect Match, 
                 Anywhere</span> in the World.
               </p>
-              <p className="w-full text-xs md:text-sm mb-2">Finding the right person is tough, but we have made it easier. Whether you’re looking for love, friendship, or chat, our algorithm will connect you with someone who shares your interests and values.</p>
+              <p className="w-full text-xs md:text-sm mb-4">Finding the right person is tough, but we have made it easier. Whether you’re looking for love, friendship, or chat, our algorithm will connect you with someone who shares your interests and values.</p>
               <a
           href="https://play.google.com/store/apps/details?id=com.baddhia.app"
           target="_blank"
         >
           <button
             type="submit"
-            className="bg-baddhia-orange p-4 xl:px-12 px-6 rounded-[16px] font-semibold xl:text-base text-[10px] text-white transition-all duration-300 hover:scale-105"
+            className="bg-baddhia-orange p-2 xl:px-12 px-6 rounded-[16px] font-semibold xl:text-base text-[10px] text-white transition-all duration-300 hover:scale-105"
           >
             Download Now
           </button>
