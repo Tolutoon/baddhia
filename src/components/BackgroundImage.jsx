7a899f1fdc41d862d037e95307fdda72c30f19b1
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-17 11:27:35 +0100

    in progress

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -4,7 +4,7 @@ import mobileBack from '/images/backAlt.png';
 
 function BackgroundImage() {
   return (
-    <div className='h-[100vh] w-full'>
+    <div className='h-[90vh] w-full mb-10'>
         <img src={background} className='h-[100vh] md:w-[100vw] object-cover md:flex hidden' loading='lazy' alt=''/>
         <img src={mobileBack} className='h-[100vh] w-[100vw] md:hidden object-cover' alt='' loading='lazy'/>
     </div>
