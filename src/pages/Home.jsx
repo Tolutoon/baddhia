eccd66d640bd6c61ee6305e570a60c6640402fb2
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-19 03:35:20 +0100

    in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,34 @@
-import React from 'react';
+import React from "react";
 
 // import components
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import FindMatch from '../components/FindMatch';
-import CardBenefits from '../components/CardBenefits';
-import Testimonials from '../components/Testimonials';
-import ConnectKindly from '../components/ConnectKindly';
-import CardBenefitsII from '../components/CardBenefitsII';
-import Press from '../components/Press';
-import LearnBlog from '../components/LearnBlog';
-import JoinCard from '../components/JoinCard';
-import Footer from '../components/Footer';
-
-
-
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import FindMatch from "../components/FindMatch";
+import CardBenefits from "../components/CardBenefits";
+import Testimonials from "../components/Testimonials";
+import ConnectKindly from "../components/ConnectKindly";
+import CardBenefitsII from "../components/CardBenefitsII";
+import Press from "../components/Press";
+import LearnBlog from "../components/LearnBlog";
+import JoinCard from "../components/JoinCard";
+import Footer from "../components/Footer";
 
 function Home() {
-
   return (
-    <main className='mx-auto transition overflow-hidden'>
-
-        <Header />
-        <Hero/>
-        <FindMatch />
-        <CardBenefits />
-        <Testimonials />
-        <ConnectKindly />
-        <CardBenefitsII />
-        <Press />
-        <LearnBlog />
-        <JoinCard/>
-        <Footer/>
+    <main className="mx-auto transition overflow-hidden">
+      <Header />
+      <Hero />
+      <FindMatch />
+      <CardBenefits />
+      <Testimonials />
+      <ConnectKindly />
+      <CardBenefitsII />
+      <Press />
+      <LearnBlog />
+      <JoinCard />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
