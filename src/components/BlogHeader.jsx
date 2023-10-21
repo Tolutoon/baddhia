50c69237b68dd072a25b22bab671c25f42315ef9
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2023-10-21 23:05:43 +0100

    Blognavbar update

--- src/components/BlogHeader.jsx
+++ src/components/BlogHeader.jsx
@@ -1,52 +1,91 @@
-import React from 'react'
-//import Logo from "/images/baddhia-logo.png"
-import Logo from "/images/baddhia-logo.png"
-import Bg from "/images/blog-header-img.png"
-import { useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// import assets
+import Logo from "/images/baddhia-Logo.png";
+
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function BlogHeader() {
-    const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
-    <>
-    <header className="w-full bg-white font-semibold  z-10 transition-all duration-300">
-      <div className="flex justify-between items-center py-4 xl:px-40 px-10">
-        <div className="cursor-pointer">
+    <header className="w-full bg-white shadow-md font-semibold fixed z-10 transition-all duration-300">
+      <div
+        className={`flex ${
+          toggleMenu ? "" : "justify-between"
+        } items-center md:py-4 xl:px-40 px-10 py-2`}
+      >
+        <Link to={"/"} className="cursor-pointer">
           <img src={Logo} alt="" style={{ width: 51, height: 50 }} />
-        </div>
+        </Link>
 
         <nav>
-          <ul className="md:flex gap-x-7 uppercase hidden transition">
+          <ul className="md:flex gap-x-7 uppercase hidden transition list-none">
             <li className="text-baddhia-orange text-base">
-              <a href="/about">dating advice</a>
+              <a href="">Dating advice</a>
             </li>
             <li className="text-baddhia-orange text-base">
-              <a href="/support">baddhia tips</a>
+              <a href="">Baddhia tips</a>
             </li>
             <li className="text-baddhia-orange text-base">
-              <a href="/">about us</a>
+              <a
+                href="/about"
+              >
+                About us
+              </a>
             </li>
             <li className="text-baddhia-orange text-base">
-              <a href="/">sign up</a>
+              <a
+                href=""
+              >
+                sign up
+              </a>
             </li>
           </ul>
           <div className="flex relative">
-            {toggleMenu ? <AiOutlineClose fontSize={28}/> : <BiMenuAltRight fontSize={28} className="md:hidden text-baddhia-orange cursor-pointer"/>}
+            {toggleMenu ? (
+              <AiOutlineClose
+                fontSize={28}
+                className="text-baddhia-orange md:hidden max-md:hidden cursor-pointer"
+                onClick={() => setToggleMenu(false)}
+              />
+            ) : (
+              <GiHamburgerMenu
+                fontSize={28}
+                className="md:hidden text-baddhia-orange cursor-pointer"
+                onClick={() => setToggleMenu(true)}
+              />
+            )}
             {toggleMenu && (
-              <ul className="flex flex-col gap-x-7 uppercase transition">
-                <li className="text-baddhia-orange text-base">
-                  <a href="/about">dating advice</a>
+              <ul className="z-30 fixed -top-0 -right-2 p-3 w-[70vw] h-full shadow-2xl md:hidden list-none uppercase bg-white
+              flex flex-col justify-start items-end rounded-md text-baddhia-orange animate-slide-in">
+                <AiOutlineClose
+                  fontSize={28}
+                  onClick={() => setToggleMenu(false)}
+                  className="cursor-pointer text-baddhia-orange mr-6 mt-2"
+                />{" "}
+                <li className="text-baddhia-orange text-base w-full my-2 mt-4 flex items-end">
+                  <a href="">Dating advice</a>
                 </li>
-                <li className="text-baddhia-orange text-base">
-                  <a href="/support">baddhia tips</a>
+                <li className="text-baddhia-orange text-base w-full my-2 flex items-end">
+                  <a href="">baddhia tips</a>
                 </li>
-                <li className="text-baddhia-orange text-base">
-                  <a href="/">about us</a>
+                <li className="text-baddhia-orange text-base w-full my-2 flex items-end">
+                  <a
+                    href="/about"
+                  >
+                    about us
+                  </a>
                 </li>
-                <li className="text-baddhia-orange text-base">
-                  <a href="/">sign up</a>
+                <li className="text-baddhia-orange text-base w-full my-2 flex items-end">
+                  <a
+                    href=""
+                  >
+                    Sign up
+                  </a>
                 </li>
               </ul>
             )}
@@ -54,12 +93,7 @@ function BlogHeader() {
         </nav>
       </div>
     </header>
-
-    
-    
-    </>
-    
-  )
+  );
 }
 
-export default BlogHeader
+export default BlogHeader;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/Header';
+import BlogHeader from '../components/BlogHeader';
 import BlogImg from '../components/BlogImg';
 import BlogWriteup from '../components/BlogWriteup';
 import BlogCommunity from '../components/BlogCommunity';
@@ -11,7 +11,7 @@ function Blog() {
     
   return (
     <>
-      <Header />
+      <BlogHeader />
       <BlogImg />
       <BlogWriteup />
       <BlogCommunity />
