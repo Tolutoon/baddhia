815e0834e2364c43b28ce7586e20f653676c2b17
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-21 17:34:09 +0100

    in progress

--- src/components/BlogWriteup.jsx
+++ src/components/BlogWriteup.jsx
@@ -1,112 +1,136 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Group4 from "/images/group4.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import Group4 from "/images/group4.jpg";
 
 function BlogWriteup() {
   return (
     <div>
-        <div className='m-0 pt-[90px] md:m-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
+      <div className="m-0 px-5 pt-[90px] md:m-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/BlogContent">Read more</Link>
+              </div>
             </div>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                <div className='rounded-2xl overflow-hidden md:block m-1'>
-                    <img className='object-cover w-full h-[232px]' src={Group4} alt="" />
-                    <div className='bg-B-writeup py-3 pl-6 pr-4'>
-                    <h1 className='font-bold font-base'>
-                        More Than Romance: Building Lasting Relationships on Baddhia
-                    </h1>
-                    <p className='text-xs mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ratione quam, eligendi eum debitis neque quod?</p>
-                    <h2 className="font-bold mt-3">Anchor's Name</h2>
-                    <div className="flex justify-between mt-2">
-                        <p className="text-xs">DD-MM-YYYY</p>
-                        <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
-                            <Link to="/BlogContent">Read more</Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+          </div>
         </div>
-        <div className='flex justify-center items-center'><button className='bg-baddhia-orange py-2 px-9 mb-28 uppercase text-white text-lg font-semibold rounded-full'><Link to="/BlogContent">Read more article</Link></button></div>
+
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/">Read more</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-white rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/">Read more</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/">Read more</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/">Read more</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-2xl overflow-hidden md:block m-1">
+          <img className="object-cover" src={Group4} alt="" />
+          <div className="bg-B-writeup py-3 pl-6 pr-2">
+            <h1 className="font-bold font-base">
+              More Than Romance: Building Lasting Relationships on Baddhia
+            </h1>
+            <p className="text-xs mt-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              ratione quam, eligendi eum debitis neque quod?
+            </p>
+            <h2 className="font-bold mt-3">Anchor's Name</h2>
+            <div className="flex justify-between mt-2">
+              <p className="text-xs">DD-MM-YYYY</p>
+              <div className="bg-baddhia-orange flex-shrink-0 border text-xs text-gray-300 rounded-full uppercase py-2 px-6 m-0">
+                <Link to="/">Read more</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <button className="bg-baddhia-orange py-2 px-9 mb-28 uppercase text-white text-lg font-semibold rounded-full">
+          <Link to="/BlogContent">Read more article</Link>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BlogWriteup
+export default BlogWriteup;
