5d0122422cc08a7af5d6d399ca2d0e8d20593f3f
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-27 19:27:16 +0100

    create-next-app

--- src/components/BlogArticle.jsx
+++ src/components/BlogArticle.jsx
@@ -101,59 +101,47 @@ function BlogArticle() {
             </div>
           </div>
 
-          <div className="mb-7">
-            <p className="text-baddhia-orange font-semibold mb-5 text-lg">
-              Related Articles
-            </p>
-            <div className="w-full rounded-2xl overflow-hidden shadow-lg">
-              <img className="h-60 w-full object-cover" src={group4} alt="" />
-              <div className="px-10 py-5 bg-orange-100 lg:h-64 w-auto md:h-80 sm:h-64">
-                <h3 className="font-bold text-lg mb-3">
-                  More Than Romance: Building Lasting Friendships on Baddhia
-                </h3>
-                <p className="text-sm mb-6">
-                  Aenean eleifend ante maecenas pulvinar montes lorem et pede
-                  dis dolor pretium donec dictum. Vici consequat justo enim.
-                  Venenatis eget adipiscing luctus lorem.
-                </p>
-                <div className="flex justify-between items-center">
-                  <div>
-                    <h4 className="font-bold mb-2">Fletcher James</h4>
-                    <p>June 28, 2018</p>
-                  </div>
-                  <div>
-                    <button className="text-white bg-baddhia-orange py-2 px-6 rounded-full">
-                      Read More
-                    </button>
-                  </div>
+          <div className='mb-7'>
+            <p className='text-baddhia-orange font-semibold mb-5 text-lg'>Related Article</p>
+            <div className='w-full rounded-2xl overflow-hidden shadow-lg'>
+              <img className='h-60 w-full object-cover' src={group4} alt="" />
+              <div className='px-10 py-5 bg-orange-100 lg:h-64 w-auto md:h-80 sm:h-64'>
+              <h3 className='font-bold text-lg mb-3'>More Than Romance: Building Lasting Friendships on Baddhia</h3>
+              <p className='text-sm mb-6'>Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem.
+              </p>
+              <div className='flex justify-between items-center'>
+                <div>
+                  <h4 className='font-bold mb-2'>
+                    Fletcher James
+                  </h4>
+                  <p>June 28, 2018</p>
                 </div>
+                <div>
+                  <button className='text-white bg-baddhia-orange py-2 px-6 rounded-full'>Read More</button>
+                </div>
+              </div>
               </div>
             </div>
           </div>
 
           <div>
-            <div className="w-full rounded-2xl overflow-hidden shadow-lg">
-              <img className="h-60 w-full object-cover" src={group4} alt="" />
-              <div className="px-10 py-5 lg:h-64 bg-orange-100 w-auto md:h-80 sm:h-64">
-                <h3 className="font-bold text-lg mb-5">
-                  More Than Romance: Building Lasting Friendships on Baddhia
-                </h3>
-                <p className="text-sm mb-6">
-                  Aenean eleifend ante maecenas pulvinar montes lorem et pede
-                  dis dolor pretium donec dictum. Vici consequat justo enim.
-                  Venenatis eget adipiscing luctus lorem.
-                </p>
-                <div className="flex justify-between items-center">
-                  <div>
-                    <h4 className="font-bold mb-2">Fletcher James</h4>
-                    <p>June 28, 2018</p>
-                  </div>
-                  <div>
-                    <button className="text-white bg-baddhia-orange py-2 px-6 rounded-full">
-                      Read More
-                    </button>
-                  </div>
+          <div className='w-full rounded-2xl overflow-hidden shadow-lg'>
+              <img className='h-60 w-full object-cover' src={group4} alt="" />
+              <div className='px-10 py-5 lg:h-64 bg-orange-100 w-auto md:h-80 sm:h-64'>
+              <h3 className='font-bold text-lg mb-5'>More Than Romance: Building Lasting Friendships on Baddhia</h3>
+              <p className='text-sm mb-6'>Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem.
+              </p>
+              <div className='flex justify-between items-center'>
+                <div>
+                  <h4 className='font-bold mb-2'>
+                    Fletcher James
+                  </h4>
+                  <p>June 28, 2018</p>
                 </div>
+                <div>
+                  <button className='text-white bg-baddhia-orange py-2 px-6 rounded-full'>Read More</button>
+                </div>
+              </div>
               </div>
             </div>
           </div>
