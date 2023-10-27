5c0d9b25caa1ee2c65105698ae962f6de6af9509
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-27 02:59:36 +0100

    create-next-app

--- src/components/ConnectKindly.jsx
+++ src/components/ConnectKindly.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ConnectKindly() {
   return (
     
-    <div className='h-[30vh] md:h-[70vh] w-full backBanner bg-center flex flex-col md:justify-center items-center pt-11 text-white md:pt-0 mb-[-22px] md:pb-[80px] md:mb-[72px]'>
+    <div className='h-[30vh] md:h-[70vh] w-full backBanner bg-center flex flex-col md:justify-center items-center pt-11 text-white md:pt-0 mb-[-42px] md:pb-[80px] md:mb-[72px]'>
       <h2 className=' md:text-6xl text-4xl md:mb-4 font-semibold'>Connect Kindly</h2>
       <p className='tracking-wide md:tracking-wider md:text-base text-[12px]'>Be the reason someone smiles today</p>
     </div>
