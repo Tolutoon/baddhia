542a35f9df17dc34b019167a8c98ab4e20d466f7
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-28 12:23:35 +0100

    push

--- src/components/BlogImg.jsx
+++ src/components/BlogImg.jsx
@@ -12,7 +12,7 @@ function BlogImg() {
 
   return (
     <div className="embla" ref={emblaRef}>
-      <div className="embla__container w-full h-full md:h-img-h ">
+      <div className="embla__container w-full h-full md:h-[500px] ">
         <div className="embla__slide relative w-full h-80 md:h-full">
         <img src={blogImg} alt="" />
         <div className='absolute left-6 md:left-10 bottom-12 inset-0 flex items-end justify-left text-left text-sm font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
