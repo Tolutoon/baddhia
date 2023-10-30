f8334721196149bfc463f6e10360d74141ce16df
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-10-30 09:57:09 +0100

    push

--- src/components/BlogImg.jsx
+++ src/components/BlogImg.jsx
@@ -12,24 +12,24 @@ function BlogImg() {
 
   return (
     <div className="embla" ref={emblaRef}>
-      <div className="embla__container w-full h-full md:h-[500px] ">
+    <div className="embla__container w-full h-full md:h-img-h ">
+    <div className="embla__slide relative h-full md:h-full"><img src={carly} alt="" />
+      <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-xs font-bold sm:left-10 sm:text-[16px] text-white lg:text-2xl'>
+      <p className='md:w-[600px] pr-5 flex flex-wrap'>Mastering Baddhia: A Comprehensive Guide to Navigating the App</p>
+      </div>
+      </div>
       <div className="embla__slide relative h-full md:h-full"><img src={carly} alt="" />
-        <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-sm font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
-        <p >Mastering Baddhia: A Comprehensive Guide to <br></br> Navigating the App</p>
-        </div>
-        </div>
-        <div className="embla__slide relative h-full md:h-full"><img src={carly} alt="" />
-        <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-sm font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
-        <p >Finding Your Perfect Match: A Guide to <br></br>Baddhia's Compatibility Features</p>
-        </div>
-        </div>
-        <div className="embla__slide relative h-full md:h-full"><img src={nathan} alt="" />
-        <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-sm font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
-        <p >More than Romance: Building Lasting Friendships <br></br>on Baddhia </p>
-        </div>
-        </div>
+      <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-xs font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
+      <p className='md:w-[600px] pr-5 flex flex-wrap'>Finding Your Perfect Match: A Guide to Baddhia's Compatibility Features</p>
+      </div>
+      </div>
+      <div className="embla__slide relative h-full md:h-full"><img src={nathan} alt="" />
+      <div className='absolute left-6 md:left-10 bottom-7 inset-0 flex items-end justify-left text-left text-xs font-bold sm:left-10 md:text-xl text-white lg:text-2xl'>
+      <p className='md:w-[700px] pr-5 flex flex-wrap'>More than Romance: Building Lasting Friendships on Baddhia Navigating the App</p>
+      </div>
       </div>
     </div>
+  </div>
     
   )
 }
