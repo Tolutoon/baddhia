6744f910c272960672d00a0a7bd4dfca52544ddb
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-11-05 16:46:03 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
+    outDir: 'dist',
+    rollupOptions: {
+      input: ['src/main.jsx', './index.html']
+    }
   },
 })
